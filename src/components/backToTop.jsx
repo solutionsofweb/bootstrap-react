3f861ad29f313147fa1c8f247574a4ba4ae069e7
saifullah <solutionsofweb@gmail.com>
2022-04-15 23:57:15 +0500

    components created

---
+++ src/components/backToTop.jsx
@@ -0,0 +1,9 @@
+const BackToTop = () => {
+    return ( 
+        <a href="#" className="back-to-top d-flex align-items-center justify-content-center">
+            <i className="bi bi-arrow-up-short" />
+        </a>
+     );
+}
+ 
+export default BackToTop;
---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return ( 
+        <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="hero-container" data-aos="fade-in">
+                <h1>Alex Smith</h1>
+                <p>I'm <span className="typed" data-typed-items="Designer, Developer, Freelancer, Photographer" /></p>
+            </div>
+        </section>
+     );
+}
+ 
+export default Home;
