4a75ae630d237e690a68bdbcabe280c7ec922ffb
saifullah <solutionsofweb@gmail.com>
2022-04-17 02:42:27 +0500

    skills in progress

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,69 +2,34 @@ import React, { Component, useEffect } from 'react';
 
 const Skills = () => {
     useEffect(() => {
-      // let skilsContent = select('skills-content');
-      // if (skilsContent) {
-      //   new Waypoint({
-      //     element: skilsContent,
-      //     offset: '80%',
-      //     handler: function(direction) {
-      //       let progress = select('.progress .progress-bar', true);
-      //       progress.forEach((el) => {
-      //         el.style.width = el.getAttribute('aria-valuenow') + '%'
-      //       });
-      //     }
-      //   })
-      // }
+      let skillsContent = [{skill: "html", level: 100}, {skill: "css", level: 90}];
+      
     }, [])
 
     return ( 
         <section id="skills" className="skills section-bg">
             <div className="container">
                 <div className="section-title">
-                <h2>Skills</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                  <h2>Skills</h2>
+                  <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
                 </div>
                 <div className="row skills-content">
-                <div className="col-lg-6" data-aos="fade-up">
-                    <div className="progress">
-                    <span className="skill">HTML <i className="val">100%</i></span>
-                    <div className="progress-bar-wrap">
-                        <div className="progress-bar" role="progressbar" aria-valuenow={100} aria-valuemin={0} aria-valuemax={100} style={{width: "100%"}} />
-                    </div>
-                    </div>
-                    <div className="progress">
-                    <span className="skill">CSS <i className="val">90%</i></span>
-                    <div className="progress-bar-wrap">
-                        <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
-                    </div>
-                    </div>
-                    <div className="progress">
-                    <span className="skill">JavaScript <i className="val">75%</i></span>
-                    <div className="progress-bar-wrap">
-                        <div className="progress-bar" role="progressbar" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100} />
-                    </div>
-                    </div>
-                </div>
-                <div className="col-lg-6" data-aos="fade-up" data-aos-delay={100}>
-                    <div className="progress">
-                    <span className="skill">PHP <i className="val">80%</i></span>
-                    <div className="progress-bar-wrap">
-                        <div className="progress-bar" role="progressbar" aria-valuenow={80} aria-valuemin={0} aria-valuemax={100} />
-                    </div>
-                    </div>
-                    <div className="progress">
-                    <span className="skill">WordPress/CMS <i className="val">90%</i></span>
-                    <div className="progress-bar-wrap">
-                        <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} />
-                    </div>
-                    </div>
-                    <div className="progress">
-                    <span className="skill">Photoshop <i className="val">55%</i></span>
-                    <div className="progress-bar-wrap">
-                        <div className="progress-bar" role="progressbar" aria-valuenow={55} aria-valuemin={0} aria-valuemax={100} />
-                    </div>
-                    </div>
-                </div>
+                  <div className="col-lg-6" data-aos="fade-up">
+                      <div className="progress">
+                        <span className="skill">HTML <i className="val">100%</i></span>
+                        <div className="progress-bar-wrap">
+                            <div className="progress-bar" role="progressbar" aria-valuenow={100} aria-valuemin={0} aria-valuemax={100} style={{width: "100%"}} />
+                        </div>
+                      </div>
+                  </div>
+                  <div className="col-lg-6" data-aos="fade-up">
+                      <div className="progress">
+                        <span className="skill">CSS <i className="val">90%</i></span>
+                        <div className="progress-bar-wrap">
+                            <div className="progress-bar" role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100} style={{width: "75%"}} />
+                        </div>
+                      </div>
+                  </div>
                 </div>
             </div>
         </section>
