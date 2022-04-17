ec566842c2faf437e8ebd61a55b69f871f9fb868
saifullah <solutionsofweb@gmail.com>
2022-04-17 23:46:26 +0500

    social link component created

---
+++ src/components/socialLink.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SocialLink = (props) => {
+    const {platformLink, platformIcon} = props;
+    const setIcon = platformIcon ? "bx bxl-" + platformIcon : "bx bxl";
+    return ( 
+        <a href={platformLink} className="twitter" target="_blank"><i className={setIcon} /></a>
+     );
+}
+ 
+export default SocialLink;
