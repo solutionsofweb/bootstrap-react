54bf49b04d64672da13e82ae1819ef41d296ff84
saifullah <solutionsofweb@gmail.com>
2022-04-24 22:37:42 +0500

    files deleted

--- src/components/backToTop.jsx
+++
@@ -1,9 +0,0 @@
-const BackToTop = () => {
-    return ( 
-        <a href="#" className="back-to-top d-flex align-items-center justify-content-center">
-            <i className="bi bi-arrow-up-short" />
-        </a>
-     );
-}
- 
-export default BackToTop;
--- src/components/socialLink.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const SocialLink = () => {
-    return ( 
-        <a href="#" className="twitter" target="_blank"><i className="bx bxl-twitter" /></a>
-     );
-}
- 
-export default SocialLink;
